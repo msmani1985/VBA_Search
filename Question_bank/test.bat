b20f6155699c447acbb7965ff1c20f5305a2a282
msmani1980 <dds932@sdsdomain.com>
2017-03-22 16:05:45 +0530

    menu changed

---
+++ Question_bank/test.bat
@@ -0,0 +1,5 @@
+C:
+cd "C:\Subbu\VBA_Search\Question_bank"
+C:
+cd "C:\Subbu\VBA_Search\Question_bank"
+"C:\Subbu\VBA_Search\Question_bank\zip.exe" -Xr9D "C:\Subbu\VBA_Search\Question_bank\Question_bank.zip" *.* -x "zip.exe" -x "test.bat" -x "ascii.txt" -x "unzip.exe"
