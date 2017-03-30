2afadb7219fad9f5ea7fcbef0ddb3019f9c8fd4f
msmani1980 <dds932@sdsdomain.com>
2017-03-30 13:31:12 +0530

    Final build

--- Question_bank/test.bat
+++ Question_bank/test.bat
@@ -1,5 +1,5 @@
-D:
-cd "D:\Development\Impelsys\officewebservices\LWW\VBA_Search\VBA_Search\Question_bank"
-D:
-cd "D:\Development\Impelsys\officewebservices\LWW\VBA_Search\VBA_Search\Question_bank"
-"D:\Development\Impelsys\officewebservices\LWW\VBA_Search\VBA_Search\Question_bank\zip.exe" -Xr9D "D:\Development\Impelsys\officewebservices\LWW\VBA_Search\VBA_Search\Question_bank\Question_bank.zip" *.* -x "zip.exe" -x "test.bat" -x "ascii.txt" -x "unzip.exe"
+C:
+cd "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank"
+C:
+cd "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank"
+"C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\zip.exe" -Xr9D "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\Question_bank.zip" *.* -x "zip.exe" -x "test.bat" -x "ascii.txt" -x "unzip.exe"
