556203aa991468304c815c7d07dc007c8eb86bd8
msmani1980 <dds932@sdsdomain.com>
2017-03-30 17:25:28 +0530

    Final delivery for phase 1 sprint 6.1

---
+++ Question_bank/deliveryfile/test.bat
@@ -0,0 +1,5 @@
+C:
+cd "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\deliveryfile"
+C:
+cd "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\deliveryfile"
+"C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\deliveryfile\zip.exe" -Xr9D "C:\Subbu\VBA_Search\gitpull\VBA_Search\Question_bank\deliveryfile\Question_bank.zip" *.* -x "zip.exe" -x "test.bat" -x "ascii.txt" -x "unzip.exe"
